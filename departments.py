d971fda3db45df1bb57d122bbe6f27af389a2d2e
Kemal Karagöz <52218326+kkrgzz@users.noreply.github.com>
2021-12-16 06:17:35 +0300

    Create departments.py
    List of Departments to scrape them all.

---
+++ departments.py
@@ -0,0 +1,12 @@
+def get_departments():
+    departments = {
+        "eee": "10056",
+        "computer_science": "10024",
+        "mechanical": "10141",
+        "civil": "10110",
+        "mechatronics": "10152",
+        "biomedical": "10032",
+        "industrial": "10059"
+    }
+
+    return departments
